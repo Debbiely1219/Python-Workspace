1c0ee1c09b46442f22cde05c9a77dfa30a92150d
Debbie Ly <71565388+Debbiely1219@users.noreply.github.com>
2020-10-10 12:34:48 -0500

    Release four assignment

---
+++ variables/practice.py
@@ -0,0 +1,97 @@
+'''
+
+[name] = [data] 
+
+
+'''
+
+#Integer
+
+a = 5 
+a = 4 
+
+#Float
+
+a = 3.45
+a = 2.1 
+
+#String
+
+a = "string"
+name = "debbie :) ly"
+
+#Boolean
+
+a = True
+a = False 
+
+#list 
+
+a = [3, 4, 4, 5 ]
+
+a = ["stringOne", "stringTwo"]
+
+a = [True, False, True]
+
+a = [1.1, 2.2, 3.3]
+
+
+
+varA = 1
+vara = 2
+
+print(varA)
+
+print (a)
+
+var = 4 
+var = "string"
+
+varTwo = var
+print (varTwo)
+
+b = [a, varA, var]
+
+list = "a", "b", "c"
+
+print (list)
+print (list [0])
+
+listVar = list[0]
+print(listVar)
+
+
+string = "debbie"
+
+print(string[0])
+
+print (5)
+print ("string")
+print (True)
+print (list)
+print (string)
+
+'''
+Syntax
+
+Logical
+'''
+
+var = 4 
+name = 4
+
+name = "debbie" 
+
+varThree = 0
+var = varThree
+
+
+debbie = "name"
+print(debbie)
+
+
+
+titles = "debbie"
+
+
+print (titles)
