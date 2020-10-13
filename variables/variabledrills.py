f42f51c49034cff00b2d01bdf2b348f604e75e1d
Debbie Ly <71565388+Debbiely1219@users.noreply.github.com>
2020-10-13 15:03:02 -0500

    variableDrills

---
+++ variables/variabledrills.py
@@ -0,0 +1,115 @@
+'''
+
+[name] = [data]
+
+
+'''
+#String 
+
+d = "string"
+
+name = "debbie ly"
+
+print(name)
+
+
+
+#Integer 
+
+var1 = 5 
+print(var1)
+
+var2 = 10
+print(var2)
+
+var3 = var2
+print(var3)
+
+x = 24 
+print(x)
+
+y = 100 
+print(y)
+
+
+
+#Floats 
+
+z = 1.1
+print (z)
+
+a = 2.75 
+print(a)
+
+b = a 
+print(b)
+
+c = 100.1
+print (c)
+
+d = 12.3 
+print (d)
+
+
+
+#Strings
+
+name = "Debbie Ly"
+print(name)
+
+e = "d"
+print(e)
+
+f = "l"
+print(f)
+
+g = "chicken"
+print(g)
+
+h = "deb" 
+print(h)
+
+
+
+#Boolean 
+
+done = False 
+print(done)
+
+smart = True
+print(smart)
+
+i = done 
+print(i)
+
+j = False 
+print(j)
+
+almostDone = True 
+print(almostDone)
+
+
+
+#list 
+
+listInt = "1", "2", "3", "4", "5"
+print (listInt [2])
+
+listString = "debbie", "khampanya", "ly", "was", "here"
+print (listString [4])
+
+listBoolean = "True", "False", "True", "True", "False"
+print (listBoolean[0])
+print (listBoolean[1])
+print (listBoolean[2])
+print (listBoolean[3])
+print (listBoolean[4])
+
+
+
+
+
+
+
+
+
