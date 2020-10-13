7111f84e4be83347c4da5c1c82a2f79bd58c866b
Debbie Ly <71565388+Debbiely1219@users.noreply.github.com>
2020-10-13 15:26:32 -0500

    variableOutline

---
+++ variables/variableOutline.py
@@ -0,0 +1,51 @@
+'''
+
+[name] = [data]
+
+'''
+
+#Integer 
+
+d = 16
+
+#Float
+
+d = 16.10
+
+#String
+
+d = "debbie is 16 years and 10 months"
+
+#Boolean
+
+d = True
+
+#List 
+
+d = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+
+#Integer 
+a=16
+A=17
+
+#String
+d = "changed to these wordings"
+
+a = d 
+
+#List
+
+newList = "1", "2", "3"
+a = (newList[1])
+print(a)
+
+#String 
+
+d = "d","e","b","b","i","e","l","y"
+z = d[0]
+print(z)
+
+print(A)
+
+a == 17 
+
