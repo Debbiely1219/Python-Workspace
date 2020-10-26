a823e66f1357eaef1d5d97b36f9de80b7805e7d9
Debbie Ly <71565388+Debbiely1219@users.noreply.github.com>
2020-10-26 15:26:27 -0500

    inputdrills

---
+++ input/inputdrills.py
@@ -0,0 +1,12 @@
+favAnimal = input("what is your favorite animal?")
+
+favNum = input("what is your favorite number?")
+#what is converting string variable to int mean 
+
+school = input("what is your school's name?")
+
+instructor = input("what is your instructors name?")
+
+ans = input("what is 2+2 ?")
+#what is converting string variable to int mean
+
