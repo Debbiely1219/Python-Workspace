c5c4f9414e7ff21545ab15c47da845bb83d90cc8
Debbie Ly <71565388+Debbiely1219@users.noreply.github.com>
2020-11-05 12:39:05 -0600

    drillcomparisonoperator

---
+++ comparisonoperators/drillcomarisonoperators.py
@@ -0,0 +1,57 @@
+''' i added print after everyone to make sure it was correct.
+if you would like me to remove and resubmit the assignment
+please let me know 
+'''
+
+b = "dog" == "cat"
+print(b)
+
+c = (1.24 == 1.25)
+print(c)
+
+d = True == False 
+print(d)
+
+e = [1,2,3] == [1,2,3]
+print(e)
+
+f = 1 <= 2 
+print(f)
+
+g = 1 >= 2 
+print(g)
+
+h = 1.66 >= 1.67 
+print(h)
+
+i = 1.66 <= 1.67 
+print(i)
+
+j = 1 < 2 
+print(j)
+
+k = 1 > 2 
+print(k)
+
+l = 11/3 < 100/33
+print(l)
+
+m = 9*7 > 8*8
+print(m)
+
+squared = 1.15*1.15
+sqrtthree = 1.732050808
+n = sqrtthree < squared 
+print(n)
+
+o = 8*3
+p = 56/2
+q = o > p 
+print(q)
+
+r = 9.797958971
+s = 73.96
+t = r >= s 
+print(t)
+
+
