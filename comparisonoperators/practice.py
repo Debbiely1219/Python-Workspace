7fd41fe14f5f67962f4050acb5cf774126c73b9a
Debbie Ly <71565388+Debbiely1219@users.noreply.github.com>
2020-11-05 12:01:05 -0600

    practicecomparisonoperator

---
+++ comparisonoperators/practice.py
@@ -0,0 +1,67 @@
+'''
+Created on Nov 5, 2020
+
+@author: ITAUser
+
+== 
+<
+>
+<=
+>=
+
+'''
+
+4 == 4 
+#True
+
+x = 4 
+
+x == 4 
+#True
+
+2 == 4
+#False 
+
+2 < 3 
+#True 
+
+3 < 2 
+#False 
+
+4 > 3 
+#True 
+
+3 > 4 
+#False 
+
+3 <= 4 
+#True 
+
+4 <= 4 
+#True 
+
+4 >= 3
+#True 
+
+4 >= 4
+#True 
+
+3 >= 4 
+#False 
+
+v = (4 == 5)
+print(v)
+
+3 >= 4 
+
+x = "michael" == "tenzin"
+print(x)
+
+x = "michael" == "Michael"
+print(x)
+
+x = "michael" == "michael"
+print(x)
+
+
+
