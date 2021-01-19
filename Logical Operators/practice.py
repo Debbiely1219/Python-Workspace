b44050660cc6dd9c7d92c1a731f3799ef2a8c36f
Debbie Ly <71565388+Debbiely1219@users.noreply.github.com>
2021-01-19 02:18:27 -0600

    logicaloperationspractice

--- Logical Operators/practice.py
+++ Logical Operators/practice.py
@@ -14,6 +14,7 @@ or
 not
 
 '''
+from builtins import True
 
 #True           #True
 4 == 4   and   5 == 5
@@ -29,5 +30,32 @@ not
 username"debbie"
 password = "Debbie1219:)"
 
-boolean = (username == "debbie")
+boolean = (username == "debbie")  or (password == "Debbie1219:)")
+print(boolean)
+
+
+2 < 3  or 5 < 10 
+#True 
+
+4 < 3 or 5 < 10
+#True 
+
+4 < 3 or 3 < 2 
+#False 
+
+not 3 == 3 
+#False 
+
+not 4 == 5 
+#True
+
+True and True
+#True 
+
+True and False 
+#False 
+
+print (False and False)
+
+
 
