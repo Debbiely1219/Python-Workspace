0c05d6d1ceec32d9a9cbe978cc1af1c63f352031
Debbie Ly <71565388+Debbiely1219@users.noreply.github.com>
2021-01-19 02:01:04 -0600

    conversion

---
+++ conversion/str.py
@@ -0,0 +1,27 @@
+'''
+Created on Jan 19, 2021
+
+@author: ITAUser
+'''
+
+intVar = 235
+stringVar = "235"
+
+print("the integer is " + str(intVar))
+print("the integer is " + stringVar)
+
+x = 3
+stringX = str(x)
+
+apples = 3 
+
+string = "I have this many apples: "
+
+sentence = string +str(apples)
+print(sentence)
+
+miles = "2"
+feet = 2000
+
+print(str(miles) + " miles converts to " + str(feet) + " feet. ")
+
