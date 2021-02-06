a02db93751c5a02f12daae91eb8f0c54ac1f1fb6
Debbie Ly <71565388+Debbiely1219@users.noreply.github.com>
2021-02-06 12:55:18 -0600

    conditionalpractice

--- Logical Operators/drills.py
+++ Logical Operators/drills.py
@@ -16,32 +16,52 @@ START HERE
 #1) Check if "dog" is equal to "cat" AND "mouse" is equal to "elephant" 
 # ) and store the answer in variable b
 
-
+dog == cat 
+mouse == elephant 
+b = False
 
 #2) Check if 1.24 is equal to 1.25 OR 1.4 is equal to 1.4 
 # ) and store the answer in variable c
 
+1.24 == 1.25 or 1.4 == 1.4 
+c = True 
+
 #3) Check if NOT(True is equal to False) and store the answer in variable d
+True == False 
+d = False 
 
 #4) Check if [1,2,3] is equal to [1,2,3] OR if ["a", "b", "c"] is equal to 
 # ) ["d", "e", "f"] and store the answer in variable e
+[1,2,3] == [1,2,3] or ["a", "b", "c"] == ["d", "e", "f"] 
+e = True
 
 #5) Check if 1 is less than or equal to 2 AND if 3 is greater than 2
 # ) and store the answer in variable f
+1 <= 2 and 3 > 2 
+f = True 
 
 #6) Check if 1 is greater than or equal to 2 OR if 3 is greater than 2
 # ) and store the answer in variable g
+1 >= 2 or 3 > 2 
+g = True
 
 #7) Check if 1.66 is greater than or equal to 1.67 AND if .5 is less than .6 
 # ) and store the answer in variable h
+1,66 > 1.67 and .5 < .6 
+h = False 
 
 #8) Check if 1.66 is less than or equal to 1.67 OR if .5 is less than .6 
 # ) and store the answer in variable i
+1.66 <= 1.67 or .5 < .6 
+i = True 
 
 #9) Check if 1 is less than 2 AND if 1 is equal to 0 
 # ) and store the answer in variable j
+1 < 2 and 1 = 0 
+j = False
 
 #10) Check if NOT(1 is greater than 2) and store the answer in variable k
+k = True
 
 '''
 For the next section, you will be reading comparison statements and determining their answer
@@ -59,75 +79,86 @@ START HERE
 '''
 
 #1) 4 == 5 and 5 == 5
+False 
 
 #2) 5 == 5 or 5 == 4
+True 
 
 #3) not 3 == 3
+True 
 
 #4) a = 2
 # ) not a == 2
-
-#5) a = 3
-# ) b = 2
-# ) a == b and b == 4
-
-#6) a = 4
-# ) b = 4
-# ) a == b or b == 4
+False 
 
 #7) a = 6
 # ) b = (12/2)
 # ) a == b and b == (24/4)
+True 
+
 
 #8) a = (3/3)
 # ) b = (13/13)
 # ) a == b or b == 1
+True 
 
 #9) a = 3
 # ) b = (9/3)
 # ) c = 2
 # ) a == b and b == c
+False 
 
 #10) a = "word"
 #  ) b = "Word"
 #  ) c = "Word"
 #  ) a == b or b == c
+True 
 
 #11) (10/5) == (12/6) and (9/3) == 3
+False 
 
 #12) (14/7) == (15/5) or (9/3) == 3
+False 
 
 #13) a = 10
 #  ) b = 5
 #  ) (a/b) == 2 and 2 == 1
+False 
 
 #14) a = 10
 #  ) b = 5
 #  ) (a/b) == (12/6)
+False 
 
 #15) a = 10
 #  ) b = 5
 #  ) c = 12
 #  ) d = 6
 #  ) (a/b) == (12/6) and (c/2) == d
+True 
 
 #16) a = "something"
 #  ) b = "some thing"
 #  ) a == b and b == 2
+False 
 
 #17) a = "word"
 #  ) b = "word"
 #  ) word = "incorrect"
 #  ) a == b and b == word
+True 
 
 #18) a = True
 #  ) a == True and not True == False
+True 
 
 #19) a = True
 #  ) b = False
 #  ) a == b or not a == b
+True 
 
 #20) a = True
 #  ) A = False
 #  ) b = False
+# this one is not complete? -DL
 #  ) A == b or a == b
---
+++ conditional/practice.py
@@ -0,0 +1,72 @@
+'''
+Created on Feb 6, 2021
+
+@author: ITAUser
+
+if/elif/else + (conditional statement):
+code block 
+
+'''
+
+x = 7 
+if (x > 0): 
+    print ("The x value is positive")
+print("test.")
+
+if (False):
+    print("This is True")
+    
+y = 6 
+
+if (y >= 0):
+    print("the y value is positive")
+else:
+    print("the y value is negative")
+    
+    
+z = 13 
+if (z < 0):
+    print ("the z value is negative.")
+elif(z == 0 ):
+    print ("the z value is zero.")
+else:
+    print("the z value is postive.")
+    
+if (z == 0):
+    #code
+    print()
+elif ( z == 1 ):
+    #code
+    print()
+elif (z == 2):
+    #code
+    print()
+else:
+    #code
+    print()
+    
+    
+password = "D3bb131y1219!"
+ 
+if(password == "adsfhg"):
+    print("you got the password")
+else: 
+    print("wrong password. try again.")
+
+if (True):
+    #code 1
+    print(1)
+elif(True):
+    #code 2  
+    print(2)
+    
+    
+    
+if (True):
+    #code 3 
+    print (3)
+if (True):
+    #code 4 
+    print (4)
+    
+      
